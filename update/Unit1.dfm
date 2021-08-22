5d653892378b1ea4cf5c22570d71b3d243307ff1
Alex <85458868+ext4n@users.noreply.github.com>
2021-08-22 11:10:08 +0300

    Add files via upload

--- update/Unit1.dfm
+++ update/Unit1.dfm
@@ -1,8 +1,9 @@
 object Form1: TForm1
-  Left = 1252
-  Top = 477
-  Width = 158
-  Height = 45
+  Left = 1335
+  Top = 979
+  BorderStyle = bsNone
+  ClientHeight = 97
+  ClientWidth = 286
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -10,7 +11,38 @@ object Form1: TForm1
   Font.Name = 'MS Sans Serif'
   Font.Style = []
   OldCreateOrder = False
+  Position = poScreenCenter
   OnCreate = FormCreate
+  OnShow = FormShow
   PixelsPerInch = 120
   TextHeight = 16
+  object Label1: TLabel
+    Left = 16
+    Top = 16
+    Width = 257
+    Height = 16
+    Alignment = taCenter
+    AutoSize = False
+    Caption = 'Updating...Please Wait!'
+  end
+  object Label2: TLabel
+    Left = 16
+    Top = 40
+    Width = 257
+    Height = 16
+    Alignment = taCenter
+    AutoSize = False
+    Caption = '5 Second Left...'
+  end
+  object ProgressBar1: TProgressBar
+    Left = 16
+    Top = 64
+    Width = 257
+    Height = 21
+    TabOrder = 0
+  end
+  object Timer1: TTimer
+    Interval = 40
+    OnTimer = Timer1Timer
+  end
 end
--- update/Unit1.pas
+++ update/Unit1.pas
@@ -4,11 +4,17 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, StdCtrls, ShellAPI;
+  Dialogs, StdCtrls, ShellAPI, ExtCtrls, ComCtrls, XPMAN, IniFiles;
 
 type
   TForm1 = class(TForm)
+    ProgressBar1: TProgressBar;
+    Label1: TLabel;
+    Timer1: TTimer;
+    Label2: TLabel;
     procedure FormCreate(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
     { Private declarations }
   public
@@ -17,19 +23,64 @@ type
 
 var
   Form1: TForm1;
-
+  Ini:TiniFile;
+  dnl : string;
 implementation
 
 {$R *.dfm}
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
-Application.ShowMainform:=false;
+Ini := TIniFile.Create(ExtractFilePath(ParamStr(0))+'set.ini');
+dnl:=Ini.ReadString('PROGRAM','UPDDWNL',dnl);
+Ini.Free;
+
+//DeleteFile('FTPSTLK.exe');
+//RenameFile('TEMP-FTPSTLK.bin', 'FTPSTLK.exe');
+//ShellExecute(0, PChar('open'), PChar(ExtractFilePath(ParamStr(0))+'FTPSTLK.exe'), nil, nil, SW_RESTORE);
+//Application.Terminate;
+//Exit;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+  ProgressBar1.Position:=ProgressBar1.Position+1;
+  if  ProgressBar1.Position=20 then
+  Label2.Caption:='4 Second Left...';
+  if  ProgressBar1.Position=40 then
+  Label2.Caption:='3 Second Left...';
+  if  ProgressBar1.Position=60 then
+  Label2.Caption:='2 Second Left...';
+    if  ProgressBar1.Position=80 then
+  Label2.Caption:='1 Second Left...';
+    if  ProgressBar1.Position=100 then
+  Label2.Caption:='Done';
+  If (ProgressBar1.Position=ProgressBar1.Max) Then
+  begin
+    Timer1.Enabled:=False;
+dnl:='0';
+Ini := TIniFile.Create(ExtractFilePath(ParamStr(0))+'set.ini');
+Ini.WriteString('PROGRAM','UPDDWNL',dnl);
+Ini.Free;
 DeleteFile('FTPSTLK.exe');
 RenameFile('TEMP-FTPSTLK.bin', 'FTPSTLK.exe');
 ShellExecute(0, PChar('open'), PChar(ExtractFilePath(ParamStr(0))+'FTPSTLK.exe'), nil, nil, SW_RESTORE);
 Application.Terminate;
 Exit;
 end;
+end;
+
+procedure TForm1.FormShow(Sender: TObject);
+begin
+if dnl<>'Yeah!' then
+begin
+dnl:='0';
+Ini := TIniFile.Create(ExtractFilePath(ParamStr(0))+'set.ini');
+Ini.WriteString('PROGRAM','UPDDWNL',dnl);
+Ini.Free;
+close;
+end;
+end;
+
 
 end.
